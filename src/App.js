965db76c690cf8b577e4857be79a60eb17520ebd
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-09-30 14:51:49 +0100

    -

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+
+function App() {
+  return (
+
+    <div class='menu'>
+
+      <div class='links'>
+
+        <a href='mailto:lorimer@wallety.org' class='link'>Email</a>
+
+        <a href='https://www.linkedin.com/in/lorimerjenkins/' class='link' target='_blank'>LinkedIn</a>
+
+        <a href='https://twitter.com/lorimer_jenkins' class='link' target='_blank'>Twitter</a>
+
+        <a href='https://www.instagram.com/lorimer_jenkins/' class='link' target='_blank'>Instagram</a>
+
+        <a href='https://github.com/LorimerJenkins' class='link' target='_blank'>GitHub</a>
+
+        <a href='https://matrix.to/#/@lorimer:matrix.org' class='link' target='_blank'>Element</a>
+    
+        <a href='https://t.me/lorimer_jenkins' class='link' target='_blank'>Telegram</a>
+
+      </div>
+
+
+    </div>
+  );
+
+}
+
+export default App;
