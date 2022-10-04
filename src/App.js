a92620a8af3a335eaab3e236a069ba074f1cd957
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-10-04 15:02:42 +0100

    -

--- src/App.js
+++ src/App.js
@@ -23,13 +23,15 @@ function App() {
         <div class='links'>
           <a href='mailto:lorimer@wallety.org' class='link'>Email</a>
           <a href='https://t.me/lorimer_jenkins' class='link' target='_blank'>Telegram</a>
-          <a href='https://www.linkedin.com/in/lorimerjenkins/' class='link' target='_blank'>LinkedIn</a>
           <a href='https://calendly.com/lorimerjenkins' class='link' target='_blank'>Calendly</a>
           <a href='https://twitter.com/lorimer_jenkins' class='link' target='_blank'>Twitter</a>
           <a href='https://github.com/LorimerJenkins' class='link' target='_blank'>GitHub</a>
+          <a href='https://keys.mailvelope.com/pks/lookup?op=get&search=lorimer@wallety.org' class='link' target='_blank'>PGP Key</a>
+          <a href='https://www.linkedin.com/in/lorimerjenkins/' class='link' target='_blank'>LinkedIn</a>
           <a href='https://matrix.to/#/@lorimer:matrix.org' class='link' target='_blank'>Element</a>
           <a href='https://www.instagram.com/lorimer_jenkins/' class='link' target='_blank'>Instagram</a>
-          <a href='https://lorimerjenkins.medium.com/' class='link'>Medium</a>
+          <a href='https://lorimerjenkins.medium.com/' class='link' target='_blank'>Medium</a>
+          <a href='https://etherscan.io/address/0xe095dd9bae4994aaaf58725d5fd011288537508f' class='link' target='_blank'>Lorimer.eth</a>
         </div>
 
 
