dc6a6391ce2416a9f4bcd13408aa353bdb915d2d
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-10-04 14:03:01 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import logo from './images/nft.jpg';
-
+import logo from './images/nft.svg';
 
 function App() {
   return (
@@ -17,28 +16,20 @@ function App() {
 
 
         <h1 class='title'><a href='/'>Lorimer Jenkins</a></h1>
-        <p class='para'>Building <a target='_blank' href='https://wallety.org'>Wallety</a></p>
+        <p class='para'>A W3 Dev/Enthusiast</p>
 
 
 
         <div class='links'>
-
           <a href='mailto:lorimer@wallety.org' class='link'>Email</a>
-
           <a href='https://t.me/lorimer_jenkins' class='link' target='_blank'>Telegram</a>
-
           <a href='https://www.linkedin.com/in/lorimerjenkins/' class='link' target='_blank'>LinkedIn</a>
-
           <a href='https://calendly.com/lorimerjenkins' class='link' target='_blank'>Calendly</a>
-
           <a href='https://twitter.com/lorimer_jenkins' class='link' target='_blank'>Twitter</a>
-
           <a href='https://github.com/LorimerJenkins' class='link' target='_blank'>GitHub</a>
-
           <a href='https://matrix.to/#/@lorimer:matrix.org' class='link' target='_blank'>Element</a>
-
           <a href='https://www.instagram.com/lorimer_jenkins/' class='link' target='_blank'>Instagram</a>
-
+          <a href='https://lorimerjenkins.medium.com/' class='link'>Medium</a>
         </div>
 
 
