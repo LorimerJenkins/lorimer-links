df72667ef36bc5020eeed2c70ce20a2dd0a36ecb
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-11-21 03:09:34 +0000

    -

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
-import logo from './images/nft.svg';
+import logo from './images/lorimer.JPG';
 
 function App() {
   return (
     <div class ='background'>
 
       <div class='links-card'>
-        
         <div class='img-wrap'>
-          <img src={logo} class='image'/>
+          <a href='/'><img src={logo} class='image'/></a>
         </div>
-
         <h1 class='title'><a href='/'>Lorimer Jenkins</a></h1>
-        <p class='para'>A Web3 Dev/Enthusiast</p>
-
+        <a href='/'><p class='para'>A Crypto Enthusiast/Dev</p></a>
         <div class='links'>
-          <a href='mailto:lorimer@wallety.org' class='link'>Email</a>
+          <a href='mailto:lorimerjenkins1@gmail.com' class='link'>Email</a>
           <a href='https://t.me/lorimer_jenkins' class='link' target='_blank'>Telegram</a>
           <a href='https://calendly.com/lorimerjenkins' class='link' target='_blank'>Calendly</a>
           <a href='https://twitter.com/lorimer_jenkins' class='link' target='_blank'>Twitter</a>
           <a href='https://github.com/LorimerJenkins' class='link' target='_blank'>GitHub</a>
-          <a href='https://nonameprotocol.com' class='link' target='_blank'>The No Name Protocol</a>
-          <a href='https://wallety.org' class='link' target='_blank'>Wallety</a>
+          <a href='https://lorimer.arweave.dev' class='link' target='_blank'>Perma Page</a>
+          <a href='https://subanalytics.org' class='link' target='_blank'>Sub Analytics</a>
           <a href='https://keys.mailvelope.com/pks/lookup?op=get&search=lorimer@wallety.org' class='link' target='_blank'>PGP Key</a>
           <a href='https://www.linkedin.com/in/lorimerjenkins/' class='link' target='_blank'>LinkedIn</a>
           <a href='https://matrix.to/#/@lorimer:matrix.org' class='link' target='_blank'>Element</a>
@@ -29,8 +26,10 @@ function App() {
           <a href='https://lorimerjenkins.medium.com/' class='link' target='_blank'>Medium</a>
           <a href='https://opensea.io/Lorimer_Jenkins' class='link' target='_blank'>Lorimer.eth</a>
         </div>
-
       </div>
+
+
+
 
     </div>
 
